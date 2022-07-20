99c3c20e31bd40379167638d09ee9cb9f48067e1
Wesley Corner <wescorner@gmail.com>
2022-07-20 02:00:11 +0000

    implemented createTweetElement in client.js to append tweet elements to tweets container

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,47 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+$(function () {
+  const tweetData = {
+    user: {
+      name: "Newton",
+      avatars: "https://i.imgur.com/73hZDYK.png",
+      handle: "@SirIsaac",
+    },
+    content: {
+      text: "If I have seen further it is by standing on the shoulders of giants",
+    },
+    created_at: 1461116232227,
+  };
+
+  const createTweetElement = function (tweetData) {
+    //return <article> html element that contains the entire HTML structure of tweet
+    const tweet = $(`
+    <article class="tweet">
+      <header>
+        <div class="name">
+          <img src="${tweetData.user.avatars}" />
+          <div>${tweetData.user.name}</div>
+        </div>
+        <div class="tag">${tweetData.user.handle}</div>
+      </header>
+      <p>${tweetData.content.text}</p>
+      <footer>
+        <p>${tweetData.created_at}</p>
+        <div class="icons">
+          <i class="fa-solid fa-flag"></i>
+          <i class="fa-solid fa-arrows-rotate"></i>              
+          <i class="fa-solid fa-heart"></i>
+        </div>
+      </footer>
+    </article>
+  `);
+    return tweet;
+  };
+
+  const $tweet = createTweetElement(tweetData);
+  const $tweet2 = createTweetElement(tweetData);
+  console.log($tweet);
+  $("#tweets-container").append($tweet);
+  $("#tweets-container").append($tweet2);
+});
